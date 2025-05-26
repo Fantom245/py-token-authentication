8cb8d49b6da266d72bd506b34dbdb43a3ffe5896
unknown <gyrkalomas@gmail.com>
2025-05-26 15:12:16 +0300

    Solution 2

--- cinema/views.py
+++ cinema/views.py
@@ -2,14 +2,9 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins
-from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly,
-    IsAuthenticated,
-    IsAdminUser
-)
+from rest_framework.permissions import IsAuthenticated
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from .permissions import IsAdminOrIfAuthenticatedReadOnly
